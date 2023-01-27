10dd5d566239a60462703b7a9c413ae8e1ee72fd
johan974 <jj974@upcmail.nl>
2023-01-27 15:14:29 +0100

    Base - security updates

--- src/main/java/app/example/WebSecurityConfig.java
+++ src/main/java/app/example/WebSecurityConfig.java
@@ -6,13 +6,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
-	
-	
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-
-
 		http.authorizeRequests().antMatchers("*").permitAll();
 	}
 
